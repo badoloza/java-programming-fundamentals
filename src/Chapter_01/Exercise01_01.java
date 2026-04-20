58ee477eb5ba40c71ebbd03d3dc3248427a84c70
baroloza <eduardoloza95@gmail.com>
2026-04-19 21:58:41 -0700

    renamed class

--- src/Chapter_01/Exercise01_01.java
+++ src/Chapter_01/Exercise01_01.java
@@ -1,6 +1,6 @@
 package Chapter_01;
 
-public class Excerise01_01 {
+public class Exercise01_01 {
     public static void main(String[] args) {
         System.out.println("initializing git repo");
     }
